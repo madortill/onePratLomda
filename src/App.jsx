b53f9a92d05f58f7b72c99bb4f842f39462a069e
madortill <mador.till@gmail.com>
2026-01-20 09:33:01 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useState } from "react";
+import "./css/App.css";
+
+function App() {
+
+  return (
+    <>
+      <div className="app">
+      hello
+      <img src="./vite.svg" alt="vite" />
+      </div>
+    </>
+  );
+}
+
+export default App;
