4eb9b32d81e9bedbaccaa9218edae47bb5bdf052
madortill <mador.till@gmail.com>
2026-05-06 18:19:21 +0300

    6.5 commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import { useState } from "react";
 import "./css/App.css";
 
+import til from "./assets/images/til.svg";
+import bahad11 from "./assets/images/bahad11.png";
+
 function App() {
 
   return (
     <>
       <div className="app">
-      hello!
-      <img src="./vite.svg" alt="vite" />
+
+        <img src={bahad11} alt="bahad6" className="bahad6" />
+        <img src={til} alt="til" className="til" />
 
       </div>
     </>
