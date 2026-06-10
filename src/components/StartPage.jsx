01bd29d39b9cc397f021ecbdd6c0d07b53a44eec
madortill <mador.till@gmail.com>
2026-06-10 11:44:53 +0300

    add enter

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -1,44 +1,64 @@
-import React from 'react'
+import React from "react";
 import { useState } from "react";
 import "../css/Start.css";
-import About from './About';
+import About from "./About";
 
 import prat1Logo from "../assets/images/pratOneLogo.svg";
 import openingGraphic from "../assets/images/openingGraphic.svg";
 
-function StartPage({changePage, setFullName}) {
-    const [canContinue, setCanContinue] = useState(false);
-    const checkHebrewName = (value) => {
+function StartPage({ changePage, setFullName }) {
+  const [canContinue, setCanContinue] = useState(false);
+  const checkHebrewName = (value) => {
+    const hebrewRegex = /^[\u0590-\u05FF]+(?:\s+[\u0590-\u05FF]+)+$/;
 
-        const hebrewRegex = /^[\u0590-\u05FF]+(?:\s+[\u0590-\u05FF]+)+$/;
+    setFullName(value);
 
-        setFullName(value);
-    
-        if (hebrewRegex.test(value.trim())) {
-            setCanContinue(true);
-        }
-        else {
-            setCanContinue(false);
-        }
-    };
-    const nextPage = () => {
-        changePage(1);
-      };
+    if (hebrewRegex.test(value.trim())) {
+      setCanContinue(true);
+    } else {
+      setCanContinue(false);
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && canContinue) {
+      nextPage();
+    }
+  };
+  const nextPage = () => {
+    changePage(1);
+  };
   return (
-    <div className='StartPage'>
-      <About/>
-      <div className='StartPage-Content'>
-        <img src={prat1Logo} alt="prat1Logo" className='prat1Logo-startPage' />
-        <p className='startTitle'>לומדת דו"ח 1</p>
-        <p className='startNote'> - הכניסו שם מלא -</p>
-        <div className='loginBox'>
-            <input type="text" placeholder='שם מלא' className='nameInput' onChange={(e) => checkHebrewName(e.target.value)} />
+    <div className="StartPage">
+      <About />
+      <div className="StartPage-Content">
+        <img src={prat1Logo} alt="prat1Logo" className="prat1Logo-startPage" />
+        <p className="startTitle">לומדת דו"ח 1</p>
+        <p className="startNote"> - הכניסו שם מלא -</p>
+        <div className="loginBox">
+          <input
+            type="text"
+            placeholder="שם מלא"
+            className="nameInput"
+            onChange={(e) => checkHebrewName(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
         </div>
-        <button className={`StartBtn ${canContinue ? "" : "StartBtnDisabled"}`} disabled={!canContinue} onClick={nextPage}>בואו נתחיל!</button>
-        <img src={openingGraphic} alt="openingGraphic" className='openingGraphic'/>
+        <button
+          className={`StartBtn ${canContinue ? "" : "StartBtnDisabled"}`}
+          disabled={!canContinue}
+          onClick={nextPage}
+        >
+          בואו נתחיל!
+        </button>
+        <img
+          src={openingGraphic}
+          alt="openingGraphic"
+          className="openingGraphic"
+        />
       </div>
     </div>
-  )
+  );
 }
 
-export default StartPage
+export default StartPage;
